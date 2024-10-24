092ad9151c4dd66dce8314de4bfe3b15bbe8953b
juanperezzdp <juanperezzdp@gmail.com>
2024-10-24 17:54:25 -0500

    config flowelcome text

--- flow/flowWelcome.js
+++ flow/flowWelcome.js
@@ -5,7 +5,7 @@ const { flowPrice } = require("./flowPrice");
 const { flowAddress } = require("./flowAddress");
 
 const flowWelcome = addKeyword(EVENTS.ACTION).addAnswer(
-  "En el salón de belleza Yuli, nos especializamos en brindarte el mejor cuidado para tu cabello y piel. \nAquí podrás encontrar una amplia gama de servicios. \n \nEscribe solamente la palabra del servicio o información que desees: \n1. *🗓️Agendar turno* \n2. *📍Dirección:* \n3. *💅Produtos* \n4. *📝Cotización*",
+  "En el salón de belleza Yuli, nos especializamos en brindarte el mejor cuidado para tu cabello y piel. \nAquí podrás encontrar una amplia gama de servicios. \n \nEscribe solamente la palabra del servicio o información que desees: \n1. *🗓️Agendar turno* \n2. *📍Dirección* \n3. *💅Produtos* \n4. *📝Cotización*",
   { capture: true },
   async (ctx, ctxFn) => {
     if (
